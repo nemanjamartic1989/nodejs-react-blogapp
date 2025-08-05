f0956a0a01061b83925b4aec6cabf9d6162afb65
Nemanja Martic <nemanja.martic@gmail.com>
2025-08-05 12:55:49 +0200

    Add router form login, register, navbar, post sections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,58 @@
-import { createBrowserRouter, RouterProvider, Route} from "react-router-dom"
+import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Write from "./pages/Write"
+import Single from "./pages/Single"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Blog App</div>
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/post/:id",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
   }
 ]);
 
 function App() {
   return (
-    <RouterProvider router={router}/>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
   )
 }
 
