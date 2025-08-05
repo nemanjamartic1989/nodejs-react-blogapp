f8caa524679767db9a22a4570110e6a784508c16
Nemanja Martic <nemanja.martic@gmail.com>
2025-08-05 14:25:19 +0200

    Introduce sass. Login & Register content and design.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='auth'>
+            <h1>Login</h1>
+            <form>
+                <input type="text" placeholder="User Name" required/>
+                <input type="password" placeholder="Password" required/>
+                <button>Login</button>
+                <span className='error-message'>This is an error</span>
+                <span>Don't you have an account? <Link to="/register">Register</Link></span>
+            </form>
+        </div>
     )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className='auth'>
+            <h1>Register</h1>
+            <form>
+                <input type="text" placeholder="User Name" required/>
+                <input type="email" placeholder="Email" required/>
+                <input type="password" placeholder="Password" required/>
+                <button>Register</button>
+                <span className='error-message'>This is an error</span>
+                <span>Do you have an account? <Link to="/login">Login</Link></span>
+            </form>
+        </div>
     )
 }
 
