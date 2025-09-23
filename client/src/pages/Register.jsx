815c07713f268162bbb844a0eee7ea519fe2ae6a
Nemanja Martic <nemanja.martic@gmail.com>
2025-09-23 13:08:51 +0200

    Introduce axios. Register form submit functionality.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
+
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate()
+
+    const handleChange = e => {
+        setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        try {
+            await axios.post("/auth/register", inputs);
+            navigate("/login")
+        } catch (err) {
+            setError(err.response.data)
+        }
+    }
+
     return (
         <div className='auth'>
             <h1>Register</h1>
             <form>
-                <input type="text" placeholder="User Name" required/>
-                <input type="email" placeholder="Email" required/>
-                <input type="password" placeholder="Password" required/>
-                <button>Register</button>
-                <span className='error-message'>This is an error</span>
+                <input type="text" name="username" placeholder="User Name" onChange={handleChange} required/>
+                <input type="email" name="email" placeholder="Email" onChange={handleChange} required/>
+                <input type="password" name="password" placeholder="Password" onChange={handleChange} required/>
+                <button onClick={handleSubmit}>Register</button>
+                {error && <p>error</p>}
                 <span>Do you have an account? <Link to="/login">Login</Link></span>
             </form>
         </div>
