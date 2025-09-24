2688ec164460ed1d2720468dbbeffc52bee8a9d5
Nemanja Martic <nemanja.martic@gmail.com>
2025-09-24 11:10:58 +0200

    Login form submit functionality.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 const Login = () => {
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: "",
+    })
+
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate()
+
+    const handleChange = e => {
+        setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        try {
+            await axios.post("/auth/login", inputs);
+            navigate("/")
+        } catch (err) {
+            setError(err.response.data)
+        }
+    }
+
     return (
         <div className='auth'>
             <h1>Login</h1>
             <form>
-                <input type="text" placeholder="User Name" required/>
-                <input type="password" placeholder="Password" required/>
-                <button>Login</button>
-                <span className='error-message'>This is an error</span>
+                <input type="text" name="username" placeholder="User Name" onChange={handleChange} required/>
+                <input type="password" name="password" placeholder="Password" onChange={handleChange} required/>
+                <button onClick={handleSubmit}>Login</button>
+                {error && <p>error</p>}
                 <span>Don't you have an account? <Link to="/register">Register</Link></span>
             </form>
         </div>
