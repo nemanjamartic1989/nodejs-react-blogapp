05ce7321caba75cfe817696dae8252675ad8a262
Nemanja Martic <nemanja.martic@gmail.com>
2025-09-24 11:12:13 +0200

    Login form submit functionality.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -16,13 +16,24 @@ export const register = (req, res) => {
 
         db.query(q, [values], (err, data) => {
             if (err) return res.json(err);
-            return res.status(200).json("User can been created."); 
+            return res.status(200).json("User can been created.")
         })
     })
 }
 
 export const login = (req, res) => {
+    // Check user:
+    const q = "SELECT * FROM users WHERE username = ?"
 
+    db.query(q, [req, body, username], (err, data) => {
+        if (err) return res.json(err)
+        if (data.length === 0) return res.status(404).json("User not found!")
+
+        // Check password:
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password)
+
+        if (!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
+    })
 }
 
 export const logout = (req, res) => {
