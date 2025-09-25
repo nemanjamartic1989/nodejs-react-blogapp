a36bc3400bd204409ba13984823de81c5c4d6df4
Nemanja Martic <nemanja.martic@gmail.com>
2025-09-25 13:34:20 +0200

    Logout using cookies.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -45,5 +45,8 @@ export const login = (req, res) => {
 }
 
 export const logout = (req, res) => {
-
+    res.clearCookie("access_token", {
+        sameSite: "none",
+        secure: true
+    }).status(200).json("User can been logged out.")
 }
