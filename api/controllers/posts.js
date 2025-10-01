a10b0b22497dd4622019d00f0e85ba8d76b4e808
Nemanja Martic <nemanja.martic@gmail.com>
2025-10-01 11:29:30 +0200

    Verifying jwt authorization on delete action.

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,10 +1,13 @@
+import { db } from "../db.js"
+import jwt from "jsonwebtoken"
+
 export const getPosts = (req, res) => {
     const q = req.query.cat
         ? "SELECT * FROM posts WHERE cat=?"
         : "SELECT * FROM posts";
 
         db.query(q, [req.query.cat], (err, data) => {
-            if (err) return res.send(err);
+            if (err) return res.status(500).send(err);
 
             return res.status(200).json(data);
         })
@@ -14,7 +17,7 @@ export const getPost = (req, res) => {
     const q = "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id = p.uid WHERE post.id = ?"
 
     db.query(q, [req.params.id], (err, data) => {
-        if (err) return res.json(err)
+        if (err) return res.status(500).json(err)
 
         return res.status(200).json(data[0])
     })
@@ -25,7 +28,22 @@ export const addPost = (req, res) => {
 }
 
 export const deletePost = (req, res) => {
-    res.json("from controller")
+    const token = req.cookies.access_token
+
+    if (!token) return res.status(401),json("Not authenticated!")
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid!")
+
+        const postId = req.params.id
+        const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
+
+        db.query(q, [postId, userInfo.id], (err, data) => {
+            if (err) return res.status(403).json("You can delete only your post!")
+
+            return res.json("Post has been deleted!")
+        })
+    })
 }
 
 export const updatePost = (req, res) => {
